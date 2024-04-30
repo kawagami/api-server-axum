a747932d230cfebd477643ca87f4429cc552011b
kawagami <joelai1988@gmail.com>
2024-04-30 23:18:31 +0800

    add: 取得所有 note 的 API 加入了類別 categories 資訊

--- src/routes/hackmd_note_lists.rs
+++ src/routes/hackmd_note_lists.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use axum::{
     extract::{Json, Path, State},
     http::StatusCode,
@@ -49,6 +51,30 @@ struct LastChangeUser {
     user_path: Option<String>,
 }
 
+#[derive(Serialize, sqlx::FromRow)]
+pub struct HackmdNoteListAndTagString {
+    id: i64,
+    title: String,
+    #[sqlx(rename = "publishLink")]
+    publish_link: String,
+    #[sqlx(rename = "lastChangedAt")]
+    last_changed_at: i64,
+    #[sqlx(rename = "readPermission")]
+    read_permission: String,
+    #[sqlx(rename = "tags")]
+    tags: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct HackmdNoteListAndCategories {
+    id: i64,
+    title: String,
+    publish_link: String,
+    last_changed_at: i64,
+    read_permission: String,
+    categories: HashSet<Option<String>>,
+}
+
 pub async fn get_note_list(
     State(state): State<SharedState>,
     Path(id): Path<i32>,
@@ -66,13 +92,61 @@ pub async fn get_note_list(
 
 pub async fn get_all_note_lists(
     State(state): State<SharedState>,
-) -> Result<Json<Vec<HackmdNoteList>>, (StatusCode, String)> {
+) -> Result<Json<Vec<HackmdNoteListAndCategories>>, (StatusCode, String)> {
     let pool = &state.read().unwrap().pool.clone();
-    let query = "select * from hackmd_note_lists";
-    let records = sqlx::query_as::<_, HackmdNoteList>(query)
+    let query = r#"
+            SELECT
+                nl.id,
+                nl.title,
+                nl."publishLink",
+                nl."lastChangedAt",
+                nl."readPermission",
+                (
+                    SELECT string_agg(t.name, ',') 
+                    FROM hackmd_note_list_tag nlt
+                    JOIN hackmd_tags t ON nlt.tag_id = t.id
+                    WHERE nlt.note_list_id = nl.id
+                ) AS tags
+            FROM
+                hackmd_note_lists nl
+            ORDER BY
+                nl."lastChangedAt" DESC;
+        "#;
+    let records = sqlx::query_as::<_, HackmdNoteListAndTagString>(query)
         .fetch_all(pool)
         .await
         .map_err(|err| (StatusCode::UNPROCESSABLE_ENTITY, err.to_string()))?;
 
-    Ok(Json(records))
+    // 建立最後要回傳的資料容器
+    let mut result: Vec<HackmdNoteListAndCategories> = Vec::new();
+
+    // 將 tags 字串處理成不重複的 HashSet
+    for record in records {
+        let categories = parse(record.tags);
+        let data = HackmdNoteListAndCategories {
+            id: record.id,
+            title: record.title,
+            publish_link: record.publish_link,
+            last_changed_at: record.last_changed_at,
+            read_permission: record.read_permission,
+            categories,
+        };
+        result.push(data);
+    }
+
+    Ok(Json(result))
+}
+
+fn parse(x: Option<String>) -> HashSet<Option<String>> {
+    match x {
+        None => HashSet::new(),
+        Some(i) => {
+            let mut set: HashSet<Option<String>> = HashSet::new();
+            for x in i.split(",").collect::<Vec<&str>>() {
+                set.insert(Some(x.to_string()));
+            }
+
+            set
+        }
+    }
 }
