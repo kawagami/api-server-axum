fa1b88a859a568ddf002dc2cb0c44e102521ffb4
kawagami <joelai1988@gmail.com>
2024-10-19 14:21:27 +0800

    update: Modify the method of obtaining the secret & reduce the JWT expiration time.

--- src/auth.rs
+++ src/auth.rs
@@ -47,30 +47,29 @@ impl IntoResponse for AuthError {
 }
 
 pub fn encode_jwt(email: String) -> Result<String, StatusCode> {
-    let jwt_token: String = "randomstring".to_string();
+    let jwt_secret = std::env::var("JWT_SECRET").expect("找不到 JWT_SECRET");
 
     let now = Utc::now();
-    let expire: chrono::TimeDelta = Duration::hours(24);
+    let expire: chrono::TimeDelta = Duration::seconds(10);
     let exp: usize = (now + expire).timestamp() as usize;
     let iat: usize = now.timestamp() as usize;
 
     let claim = Cliams { iat, exp, email };
-    let secret = jwt_token.clone();
 
     encode(
         &Header::default(),
         &claim,
-        &EncodingKey::from_secret(secret.as_ref()),
+        &EncodingKey::from_secret(jwt_secret.as_ref()),
     )
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
 
 pub fn decode_jwt(jwt: String) -> Result<TokenData<Cliams>, StatusCode> {
-    let secret = "randomstring".to_string();
+    let jwt_secret = std::env::var("JWT_SECRET").expect("找不到 JWT_SECRET");
 
     let result: Result<TokenData<Cliams>, StatusCode> = decode(
         &jwt,
-        &DecodingKey::from_secret(secret.as_ref()),
+        &DecodingKey::from_secret(jwt_secret.as_ref()),
         &Validation::default(),
     )
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR);
