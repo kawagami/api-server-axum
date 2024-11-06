e35e0230d421adef02c62d90dd832eeeedc6e16c
kawagami <joelai1988@gmail.com>
2024-11-06 20:54:46 +0800

    update: 將 UploadError 修改為全站使用的客製錯誤

--- src/errors.rs
+++ src/errors.rs
@@ -4,7 +4,7 @@ use axum::{
 };
 
 #[derive(Debug)]
-pub enum UploadError {
+pub enum AppError {
     ConnectFail,
     GetNextFieldFail,
     NotThing,
@@ -12,16 +12,16 @@ pub enum UploadError {
     InvalidJson,
 }
 
-impl IntoResponse for UploadError {
+impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
-            UploadError::ConnectFail => (StatusCode::INTERNAL_SERVER_ERROR, "圖片 server 連接失敗"),
-            UploadError::GetNextFieldFail => {
+            AppError::ConnectFail => (StatusCode::INTERNAL_SERVER_ERROR, "圖片 server 連接失敗"),
+            AppError::GetNextFieldFail => {
                 (StatusCode::BAD_REQUEST, "multipart next_field await 失敗")
             }
-            UploadError::NotThing => (StatusCode::BAD_REQUEST, "multipart next_field 沒東西"),
-            UploadError::ReadBytesFail => (StatusCode::BAD_REQUEST, "field bytes 時失敗"),
-            UploadError::InvalidJson => (StatusCode::BAD_REQUEST, "無效的 json 格式"),
+            AppError::NotThing => (StatusCode::BAD_REQUEST, "multipart next_field 沒東西"),
+            AppError::ReadBytesFail => (StatusCode::BAD_REQUEST, "field bytes 時失敗"),
+            AppError::InvalidJson => (StatusCode::BAD_REQUEST, "無效的 json 格式"),
         };
         (status, error_message).into_response()
     }
