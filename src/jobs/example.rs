b76260e1e5fc4f3cd3ae058add7ed7ef746fd89d
kawagami <joelai1988@gmail.com>
2024-12-12 14:35:55 +0800

    add: 新增 new Job 範例

---
+++ src/jobs/example.rs
@@ -0,0 +1,38 @@
+use crate::{
+    state::AppStateV2,
+    structs::{
+        jobs::AppJob,
+        ws::{ChatMessage, ChatMessageType, To},
+    },
+};
+use async_trait::async_trait;
+use chrono::Local; // 引入 chrono 來處理時間
+
+#[derive(Clone)]
+pub struct ExampleJob;
+
+#[async_trait]
+impl AppJob for ExampleJob {
+    fn cron_expression(&self) -> &str {
+        "0 * * * * *" // 每分鐘執行一次
+    }
+
+    async fn run(&self, state: AppStateV2) {
+        // 獲取當前 UTC+8 時間並格式化
+        let current_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+
+        let message = format!("{}", current_time);
+
+        let chat_message = ChatMessage::new_jsonstring(
+            ChatMessageType::Message,
+            current_time.clone(),
+            "KawaBot".to_string(),
+            To::All,
+        );
+        let _ = state.get_tx().send(chat_message);
+
+        let _ = state
+            .insert_chat_message("Message", "All", "KawaBot", &message)
+            .await;
+    }
+}
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,4 +1,8 @@
-use crate::{jobs::hackmd::FetchNotesJob, state::AppStateV2, structs::jobs::AppJob};
+use crate::{
+    jobs::{example::ExampleJob, hackmd::FetchNotesJob},
+    state::AppStateV2,
+    structs::jobs::AppJob,
+};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio_cron_scheduler::{Job, JobScheduler};
@@ -6,24 +10,37 @@ use tokio_cron_scheduler::{Job, JobScheduler};
 pub async fn initialize_scheduler(state: AppStateV2) -> Arc<Mutex<JobScheduler>> {
     let scheduler = Arc::new(Mutex::new(JobScheduler::new().await.unwrap()));
 
-    let job_state = state.clone();
-    let scheduler_clone = scheduler.clone();
+    let cloned_state = state.clone();
+    let cloned_scheduler = scheduler.clone();
 
     tokio::spawn(async move {
-        let notes_job = FetchNotesJob;
+        add_job_to_scheduler(
+            cloned_scheduler.clone(),
+            cloned_state.clone(),
+            FetchNotesJob,
+        )
+        .await;
+        add_job_to_scheduler(cloned_scheduler.clone(), cloned_state.clone(), ExampleJob).await;
 
-        let job = Job::new_async(notes_job.clone().cron_expression(), move |_uuid, _l| {
-            let job_state = job_state.clone();
-            let notes_job = notes_job.clone();
-            Box::pin(async move {
-                notes_job.run(job_state).await;
-            })
-        })
-        .unwrap();
-
-        scheduler_clone.lock().await.add(job).await.unwrap();
-        scheduler_clone.lock().await.start().await.unwrap();
+        cloned_scheduler.lock().await.start().await.unwrap();
     });
 
     scheduler
+}
+
+async fn add_job_to_scheduler<J: AppJob + Clone + Send + Sync + 'static>(
+    scheduler: Arc<Mutex<JobScheduler>>,
+    state: AppStateV2,
+    job_instance: J,
+) {
+    let job = Job::new_async(job_instance.clone().cron_expression(), move |_uuid, _l| {
+        let cloned_state = state.clone();
+        let cloned_job = job_instance.clone();
+        Box::pin(async move {
+            cloned_job.run(cloned_state).await;
+        })
+    })
+    .unwrap();
+
+    scheduler.lock().await.add(job).await.unwrap();
 }
