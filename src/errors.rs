570704937fff8b77ea397691c404fa99a100166c
kawagami <joelai1988@gmail.com>
2024-12-29 00:51:44 +0800

    update: 將 error 使用 thiserror 改寫

--- src/errors.rs
+++ src/errors.rs
@@ -2,33 +2,37 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum AppError {
+    #[error("圖片 server 連接失敗")]
     ConnectFail,
+    #[error("multipart next_field await 失敗")]
     GetNextFieldFail,
+    #[error("multipart next_field 沒東西")]
     NotThing,
+    #[error("field bytes 時失敗")]
     ReadBytesFail,
+    #[error("無效的 json 格式")]
     InvalidJson,
+    #[error("Invalid response from the server")]
     InvalidResponse,
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let (status, error_message) = match self {
-            AppError::ConnectFail => (StatusCode::INTERNAL_SERVER_ERROR, "圖片 server 連接失敗"),
-            AppError::GetNextFieldFail => {
-                (StatusCode::BAD_REQUEST, "multipart next_field await 失敗")
-            }
-            AppError::NotThing => (StatusCode::BAD_REQUEST, "multipart next_field 沒東西"),
-            AppError::ReadBytesFail => (StatusCode::BAD_REQUEST, "field bytes 時失敗"),
-            AppError::InvalidJson => (StatusCode::BAD_REQUEST, "無效的 json 格式"),
-            AppError::InvalidResponse => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Invalid response from the server",
-            ),
+        let status_code = match &self {
+            AppError::ConnectFail => StatusCode::BAD_GATEWAY,
+            AppError::GetNextFieldFail => StatusCode::BAD_REQUEST,
+            AppError::NotThing => StatusCode::NOT_FOUND,
+            AppError::ReadBytesFail => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::InvalidJson => StatusCode::UNPROCESSABLE_ENTITY,
+            AppError::InvalidResponse => StatusCode::BAD_GATEWAY,
         };
-        (status, error_message).into_response()
+
+        let error_message = self.to_string();
+        (status_code, error_message).into_response()
     }
 }
 
