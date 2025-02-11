7933092888867d37d19da8c827151b9494dcaa60
kawagami <joelai1988@gmail.com>
2025-02-12 00:44:24 +0800

    update: 將 main 調整至跟開發環境相容

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod scheduler;
 mod state;
 mod structs;
 
+use std::env::var;
 use tokio::{net::TcpListener, signal};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -25,7 +26,10 @@ async fn main() {
     let app = routes::app().await;
 
     // run it with hyper
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let host = var("APP_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = var("APP_PORT").unwrap_or_else(|_| "3000".to_string());
+    let bind_addr = format!("{}:{}", host, port);
+    let listener = TcpListener::bind(&bind_addr).await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
