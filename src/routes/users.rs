731fffb067ef4dbd8f8ea2d951d217361fbb9f1e
kawagami <joelai1988@gmail.com>
2025-02-23 00:22:05 +0800

    update: 調整 users API 回應格式

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,4 +1,4 @@
-use crate::{repositories::users, state::AppStateV2, structs::users::User};
+use crate::{errors::AppError, repositories::users, state::AppStateV2, structs::users::User};
 use axum::{extract::State, routing::get, Json, Router};
 
 pub fn new() -> Router<AppStateV2> {
@@ -6,14 +6,8 @@ pub fn new() -> Router<AppStateV2> {
 }
 
 /// 取 users 清單
-async fn get_users(State(state): State<AppStateV2>) -> Json<Vec<User>> {
-    let result = users::get_users(&state).await;
+async fn get_users(State(state): State<AppStateV2>) -> Result<Json<Vec<User>>, AppError> {
+    let result = users::get_users(&state).await.map_err(AppError::from)?;
 
-    match result {
-        Ok(users) => Json(users),
-        Err(err) => {
-            tracing::error!("{}", err);
-            Json(vec![])
-        }
-    }
+    Ok(Json(result))
 }
