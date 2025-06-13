d3eccdf68e1dda8204034ca313b6c21b97309350
kawagami <joelai1988@gmail.com>
2025-06-13 09:21:58 +0800

    feat: 調整 StockDayAllJob 執行時間

--- src/jobs/stock_day_all.rs
+++ src/jobs/stock_day_all.rs
@@ -7,7 +7,7 @@ pub struct StockDayAllJob;
 #[async_trait]
 impl AppJob for StockDayAllJob {
     fn cron_expression(&self) -> &str {
-        "0 0 17 * * *" // UTC+8 的 01:00 執行即可
+        "0 0 8,20 * * *" // UTC+8 的 16:00 & 04:00 執行
     }
 
     async fn run(&self, state: AppStateV2) {
