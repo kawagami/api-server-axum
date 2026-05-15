f4acf38a10860619aa142f0bb1af860b9ead4088
kawagami <joelai1988@gmail.com>
2026-05-16 01:01:57 +0800

    fix(migrations): use ON CONFLICT DO NOTHING for image/stock permissions

--- migrations/20260515164000_add_image_permissions.up.sql
+++ migrations/20260515164000_add_image_permissions.up.sql
@@ -1,4 +1,5 @@
 INSERT INTO permissions (resource, action, description) VALUES
     ('image', 'read',   '查看圖片列表'),
     ('image', 'write',  '上傳圖片'),
-    ('image', 'delete', '刪除圖片');
+    ('image', 'delete', '刪除圖片')
+ON CONFLICT (resource, action) DO NOTHING;
--- migrations/20260515165000_add_stock_permissions.up.sql
+++ migrations/20260515165000_add_stock_permissions.up.sql
@@ -1,3 +1,4 @@
 INSERT INTO permissions (resource, action, description) VALUES
     ('stock', 'read',  '查看股票資料'),
-    ('stock', 'write', '修改股票資料');
+    ('stock', 'write', '修改股票資料')
+ON CONFLICT (resource, action) DO NOTHING;
